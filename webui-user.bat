adcf99307b8b10e38a0ea7fa12c5fe2ff1434cf2
phong686 <78142025+phong686@users.noreply.github.com>
2025-04-21 14:42:47 +0700

    Fix

--- webui-user.bat
+++ webui-user.bat
@@ -1,8 +1,9 @@
 @echo off
 
+set PYTORCH_CUDA_ALLOC_CONF=garbage_collection_threshold:0.6,max_split_size_mb:128
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
+set COMMANDLINE_ARGS=--xformers --opt-channelslast --administrator --listen --enable-insecure-extension-access --no-hashing --api --api-auth "binhnx:admin@213"
 
 call webui.bat
